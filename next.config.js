5e347d4446f6fd36baae41b0a17568842e1d58a4
TiendaLeBonMarche <tiendalebonmarche@gmail.com>
2026-03-26 16:30:42 -0500

    cambios 26/03

--- next.config.js
+++ next.config.js
@@ -13,6 +13,21 @@ const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
  */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-DNS-Prefetch-Control", value: "on" },
+          { key: "X-XSS-Protection", value: "1; mode=block" },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" }
+        ],
+      },
+    ]
+  },
   logging: {
     fetches: {
       fullUrl: true,
