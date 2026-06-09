bbafb9cc8a3fef8ed90207b8bb7ce639ad088522
lamusj-hermes <tiendalebonmarche@gmail.com>
2026-06-09 22:27:39 +0000

    fix: redirect loop en middleware - usar NextResponse.next() en vez de redirect cuando se setea cacheId

--- src/middleware.ts
+++ src/middleware.ts
@@ -124,13 +124,13 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next()
   }
 
-  // if one of the country codes is in the url and the cache id is not set, set the cache id and redirect
+  // if one of the country codes is in the url and the cache id is not set, set the cache id and continue
   if (urlHasCountryCode && !cacheIdCookie) {
-    response.cookies.set("_medusa_cache_id", cacheId, {
+    const res = NextResponse.next()
+    res.cookies.set("_medusa_cache_id", cacheId, {
       maxAge: 60 * 60 * 24,
     })
-
-    return response
+    return res
   }
 
   // check if the url is a static asset
